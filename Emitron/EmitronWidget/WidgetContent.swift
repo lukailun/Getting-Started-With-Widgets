40faca21828e6e6742aaa89f7955ebc13c0bb149
Karen <957458041@qq.com>
2024-01-03 18:47:59 +0800

    update

--- Emitron/EmitronWidget/WidgetContent.swift
+++ Emitron/EmitronWidget/WidgetContent.swift
@@ -28,7 +28,7 @@
 
 import WidgetKit
 
-struct WidgetContent: TimelineEntry {
+struct WidgetContent: Codable, TimelineEntry {
   var date = Date()
   let name: String
   let cardViewSubtitle: String
